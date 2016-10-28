e17b239db6e5bc99a9b6afaa13082e0d4c42913d
GeneXu <genexu1117@gmail.com>
2016-10-28 09:09:23 +0800

    add pie background view to Chart

--- src/Chart.js
+++ src/Chart.js
@@ -3,14 +3,30 @@ import { StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
   chart: {
-    backgroundColor: 'white'
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pieBackground: {
+    backgroundColor: '#EFEFEF'
   }
 });
+
+const pieMargin = 10;
 
 class Chart extends Component {
   render() {
     return (
-        <View style={[styles.chart, {width: this.props.chart_w, height: this.props.chart_w}]}></View>
+        <View style={[styles.chart, {width: this.props.chart_w, height: this.props.chart_w}]}>
+            <View
+              style={[
+                styles.pieBackground,{
+                  height: this.props.chart_w - pieMargin,
+                  width: this.props.chart_w - pieMargin,
+                  borderRadius: (this.props.chart_w - pieMargin) / 2
+                }]}
+            >
+            </View>
+        </View>
     );
   }
 }
