3283917ded5afcb0dc2475a0e13b3572c801d1e3
GeneXu <genexu1117@gmail.com>
2016-11-01 22:02:28 +0800

    update test example

--- test/index.ios.js
+++ test/index.ios.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, View, StatusBar } from 'react-native';
+import { AppRegistry, StyleSheet, ScrollView , StatusBar, Text, View} from 'react-native';
 import PieChart from 'react-native-pie-chart';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ddd'
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 24,
+    margin: 10
   }
 });
 
 export default class test extends Component {
   render() {
+    const chart_wh = 300
+    const series = [333, 666, 123, 789, 537]
+    const sliceColor = ['#F44336','#2196F3','#FFEB3B', '#4CAF50', '#FF9800']
+
     return (
-      <View style={styles.container}>
-        <StatusBar
-          hidden={true}
-        />
-        <PieChart
-          chart_wh={300}
-          series={[333, 666, 123, 789, 537]}
-          sliceColor={['#F44336','#2196F3','#FFEB3B', '#4CAF50', '#FF9800']}
-        />
-      </View>
+      <ScrollView style={{flex: 1}}>
+        <View style={styles.container}>
+          <StatusBar
+            hidden={true}
+          />
+          <Text style={styles.title}>Basic</Text>
+          <PieChart
+            chart_wh={chart_wh}
+            series={series}
+            sliceColor={sliceColor}
+          />
+          <Text style={styles.title}>Doughnut</Text>
+          <PieChart
+            chart_wh={chart_wh}
+            series={series}
+            sliceColor={sliceColor}
+            doughnut={true}
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
