c35f2912f913e197135a5ed0fa183f8bee94aafc
GeneXu <genexu1117@gmail.com>
2017-04-22 21:38:36 +0800

    rename test to example, update example react-native version to 0.43.4

---
+++ example/index.ios.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, ScrollView , StatusBar, Text, View} from 'react-native';
+import PieChart from 'react-native-pie-chart';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 24,
+    margin: 10
+  }
+});
+
+export default class example extends Component {
+  render() {
+    const chart_wh = 250
+    const series = [123, 321, 123, 789, 537]
+    const sliceColor = ['#F44336','#2196F3','#FFEB3B', '#4CAF50', '#FF9800']
+
+    return (
+      <ScrollView style={{flex: 1}}>
+        <View style={styles.container}>
+          <StatusBar
+            hidden={true}
+          />
+          <Text style={styles.title}>Basic</Text>
+          <PieChart
+            chart_wh={chart_wh}
+            series={series}
+            sliceColor={sliceColor}
+          />
+          <Text style={styles.title}>Doughnut</Text>
+          <PieChart
+            chart_wh={chart_wh}
+            series={series}
+            sliceColor={sliceColor}
+            doughnut={true}
+            coverRadius={0.45}
+            coverFill={'#FFF'}
+          />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+AppRegistry.registerComponent('example', () => example);
