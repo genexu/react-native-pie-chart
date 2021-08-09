42c565ab2696677f97b2a794ae7d52b4d66c87a9
Aidin Gharibnavaz <aidin@aidinhut.com>
2021-08-10 10:10:47 +1200

    Add @react-native-community/art to dev-deps. tsc needs it.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,3 @@
 npm install
-# 'tsc' needs this package to be installed. We have it as peerDependency so it won't get install by the above command.
-npm install --no-save @react-native-community/art
 npm run check
 npm run build
