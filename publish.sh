33b97b01de7d4d92a416413f1f9027ea706bf53a
Aidin Gharibnavaz <aidin@aidinhut.com>
2021-08-20 10:02:26 +1200

    Enhance publish.sh script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+
+set -e
+
+GIT_STATUS=$(git status --porcelain=1)
+if [ -n "$GIT_STATUS" ]; then
+  echo
+  echo "The repo is not clean!"
+  exit 1
+fi
+
 npm install
 npm run check
 npm run build
+
+echo
+read -p "Publish to npmjs? (y/n) " choice
+if [ $choice != 'y' ]
+then
+  exit 0
+fi
+
+echo "Tagging git..."
+VERSION=$(node -p "require('./package.json').version")
+git tag "v""$VERSION"
+git push --tags
+
+echo "Publishing to npmjs..."
+npm login
+npm publish --access public
